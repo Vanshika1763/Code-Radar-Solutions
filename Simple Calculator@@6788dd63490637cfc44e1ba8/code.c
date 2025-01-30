b7b6f375ca42b3468eb5272f0ee3725ea1198e03
Vanshika1763 <vanshika1763.becse24@chitkara.edu.in>
2025-01-30 14:30:55 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,add,mul,div,sub;
+    scanf("%d %d",&a,&b);
+    add = a+b;
+    printf("%d",add);
+    sub=a-b;
+    printf("%d",sub);
+    mul=a*b;
+    printf("%d",mul);
+    div=a/b;
+    printf("%d",div);
+    return 0;
+}
