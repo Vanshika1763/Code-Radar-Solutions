174568ad897432d67098c9e97232a2babb5b9f73
Vanshika1763 <vanshika1763.becse24@chitkara.edu.in>
2025-01-30 14:44:37 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,add,mul,div,sub;
+    int a,b,add,sub,mul,div;
+    char oper;
     scanf("%d %d",&a,&b);
-    add=a+b;
-    printf("%d",add);
-    sub=a-b;
-    printf("%d",sub);
-    mul=a*b;
-    printf("%d",mul);
-    div=a/b;
-    printf("%d",div);
+    scanf("%c",oper);
+    if(oper==+){
+        add=a+b;
+        printf("%d",add);
+    }
+    else if(oper==-){
+        sub=a-b;
+        printf("%d",sub);
+    }
+    else if(oper==*){
+        mul=a*b;
+        printf("%d",mul);
+    }
+    else if(oper==/){
+        div=a/b;
+        printf("%d",div);
+    }
+    else{
+        printf("error");
+    }
     return 0;
 }
